906df146ac341308233ca196b486fbf45fc89387
samapth-ops <sampathkaali002@gmail.com>
2022-05-20 20:11:02 +0530

    schemeDescriptionData template added

--- src/Components/SchemeDescription/SchemeDescription.js
+++ src/Components/SchemeDescription/SchemeDescription.js
@@ -1,21 +1,42 @@
+import { SchemeDescriptionData } from "./SchemesDescriptionData";
+import { useParams } from "react-router-dom";
+import "./SchemeDescription.css";
 const SchemeDescription = () => {
+    const {id} = useParams();
+
+    function capitalizeWords(arr) {
+      return arr.map(element => {
+        return element.charAt(0).toUpperCase() + element.substring(1).toLowerCase();
+      });
+    }
+
+    const descriptionObj = SchemeDescriptionData[capitalizeWords(id.split("-")).join("")];
+
     return ( 
         <div className="scheme_description">
-          <h2>Chief Minister’s Solar Powered Green House Scheme (CMSPGHS)</h2>  
+          <h2>{descriptionObj.title}</h2>  
           <h2>Introduction</h2>
-          <p>The Chief Minister’s Solar Powered Green House Scheme (CMSPGHS) was launched during the year 2011-201. It is a flagship scheme of Government of Tamil Nadu, which fulfils the Housing requirement of the rural poor people along with Solar Powered Home Lighting Systems and thereby promoting green energy. Nowhere else in the country such a free housing scheme for the construction of houses with an area of 300 sq.ft with solar powered lighting is implemented.</p>
+          <p>{descriptionObj.intro}</p>
           <h2>Eligibility of the Beneficiaries</h2>
-          <p>You must be a resident of the village panchayat</p>
-          <p>Have clear patta for the site/house in the name of the head of the family or any other member of the household.</p>
-          <p>You must not have any other owned house/property in village or elsewhere</p>
-          <p>Should not have been benefited under any other housing scheme of the Government.</p>
-          <h2> Salient features of the Scheme</h2> 
-         <p>The poor people living in rural areas are eligible to be beneficiaries of the CMSPGHS.Each house shall be built with an area of 300 square feet at an unit cost of Rs.2.10 lakh fully funded by the State Government.
-Each house shall consist of a living room, bed room, kitchen, toilet and verandah apart from the provision of harvesting rain water.Both the construction of houses and installation of solar lights shall be taken up by the Rural Development and Panchayat Raj Department.The Green Houses shall be constructed on the land owned by the beneficiary.People with patta for their house sites are only eligible under this scheme.</p>
+          {descriptionObj.eligibilties.map((line,index)=>{
+            return(
+              <p key={index}>{line}</p>
+            )
+          })}
+          {descriptionObj.salient && <h2>Salient features of the Scheme</h2>}
+          {descriptionObj.salient && descriptionObj.salient.map((line,index)=>{
+            return(
+              <p key={index}>{line}</p>
+            )
+          })}
           <h2>Mode of Selection of Beneficiaries </h2>
-          <p>The beneficiaries should be selected from the poor people living in the Village Panchayat and the list of such beneficiaries shall be approved by the Gram Sabha.Out of the total allocation 29% shall be allotted to scheduled castes, 1% shall be allotted to Scheduled Tribes and the remaining 70% shall be allotted to others.3% of the District wise allocation should be reserved exclusively for differently abled persons.While preparing the list of beneficiaries, priority should be given to the following persons differently abled, widows, destitute and deserted women, women headed families, Ex-servicemen and retired members of Paramilitary forces, families having severely malnourished children, transgenders, HIV/AIDS/TB affected persons.
-Priority shall also be given to households having a mentally challenged person in the family and Victims of natural calamities such as fire, flood, etc.,</p>
-<h2>Rural Buildings Maintenance and Renovation Scheme (RBMRS)</h2>
+          {descriptionObj.mode.map((line,index)=>{
+            return(
+              <p key={index}>{line}</p>
+            )
+          })}
+
+{/* <h2>Rural Buildings Maintenance and Renovation Scheme (RBMRS)</h2>
 <p>In order to supplement the efforts of the rural Local Bodies in maintenance of all its assets, a new Scheme namely ‘Rural Buildings Maintenance and Renovation Scheme’s being introduced with effect from 2010-11 at an outlay of Rs.100 crores.
 
 It was introduced with an objective of routine maintenance of rural Local Bodies assets, such as Anganwadi Centers, Panchayat office buildings, Overhead tanks, Integrated Sanitary Complexes for Women, etc., for renovation under the Scheme.</p>
@@ -30,9 +51,9 @@ Repair of cremation sheds/resting sheds in Cremation/Burial Grounds.
 Maintenance and renovation of damaged OHTs / GLRs
 Major Works:
 Complete replacement of badly damaged existing tiled roofing in buildings.
-Complete replacement of Asbestos sheet roofing with new tiled roofing.</p>
+Complete replacement of Asbestos sheet roofing with new tiled roofing.</p> */}
 
-</div>
+</div> 
 
 
      
